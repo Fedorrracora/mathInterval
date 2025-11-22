10404136095fd0c5b91e3f99bef6b077bb85a60f
fedor <valyanskiy.f2009@yandex.ru>
2025-11-22 23:10:56 +0300

    add test 3

---
+++ Testing/cpp/test3.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <interval.h>
+#include <verifier.h>
+
+TEST(CPP, multi_add) {
+    interval::interval<int> a;
+    verify::line_checker line(verify::read_file("answers/test3.a"));
+    a.add_interval(1, 10);
+    a.add_point(10);
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 1: adding border point\n";
+    {
+        bool b = a.add_interval(10, 11);
+        ASSERT_TRUE(verify::same(a.print(), line(), true, true, false) && b) << "error in step 2: meet 2 intervals: last - interval\n";
+    }
+    a.add_interval(11, 20);
+    {
+        bool b = a.add_point(11);
+        ASSERT_TRUE(verify::same(a.print(), line(), true, true, false) && b) << "error in step 3: meet 2 intervals: last - point\n";
+    }
+    a.add_interval(20, 30);
+    a.add_point(30);
+    a.add_point(1);
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 4: random additions\n";
+    {
+        bool b = false;
+        b |= a.add_point(1);
+        b |= a.add_point(30);
+        b |= a.add_point(25);
+        b |= a.add_point(10);
+        b |= a.add_interval(10, 10);
+        b |= a.add_interval(10, 15);
+        b |= a.add_interval(10, 20);
+        b |= a.add_interval(1, 15);
+        b |= a.add_interval(25, 30);
+        EXPECT_TRUE(verify::same(a.print(), line.last, true, true, false) && !b) << "error in step 5: adding intervals/points that not changing data\n";
+    }
+    a.add_point(-1);
+    a.add_point(0);
+    a.add_point(31);
+    a.add_point(32);
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 6: points in left and right\n";
+    a.add_interval(-10, 1);
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 7: coating interval\n";
+    a.add_point(-15);
+    a.add_point(-14);
+    a.add_point(-13);
+    a.add_point(-12);
+    a.add_point(-11);
+    a.add_point(-10);
+    a.add_interval(-20, -15);
+    a.add_point(-21);
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 8: points formatting\n";
+    a.add_interval(interval::minimal<int>(), -21);
+    a.add_interval(35, interval::maximal<int>());
+    ASSERT_TRUE(verify::same(a.print(), line(), true, true, false)) << "error in step 9: points formatting\n";
+}
