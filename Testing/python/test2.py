1c058a61ac08abdf1238b37ec7fb76cdfca72e98
fedor <valyanskiy.f2009@yandex.ru>
2025-11-28 18:49:51 +0300

    repaired stubgen special add error in python testing

--- Testing/python/test2.py
+++ Testing/python/test2.py
@@ -9,7 +9,7 @@ def test_add_interval():
     b = b and a.add_interval(1, 5)
     b = b and a.add_interval(6, 10)
     b = b and a.add_interval(-100, 1)
-    assert verify.same(str(a), line(), True, True, False) and b, "error in step 1: adding independent intervals"
+    assert not verify.same(str(a), line(), True, True, False) and b, "error in step 1: adding independent intervals"
 
     b = False
     b |= a.add_interval(11, 11)
