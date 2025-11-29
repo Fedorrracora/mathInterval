5633d4a390e6a147a7f334fbf278c390ce294757
fedor <valyanskiy.f2009@yandex.ru>
2025-11-29 21:28:10 +0300

    change stubgen generation: try 11. I change copy path in CMakeLists.txt repaired load_stubgen.sh I did not change reference yet

--- load_stubgen.sh
+++ load_stubgen.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 curl -L -o artifact.zip "https://nightly.link/Fedorrracora/mathInterval/workflows/build-wheels/main/mathInterval.pyi.zip"
-unzip -o artifact.zip
+unzip -o artifact.zip -d src/_mathInterval
 rm -f artifact.zip
-mv mathInterval.pyi mathInterval/mathInterval.pyi
