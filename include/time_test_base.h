83f947515a95fb309c363c4c208bf905a07cf785
fedor <valyanskiy.f2009@yandex.ru>
2025-12-18 01:13:48 +0300

    small repair

--- include/time_test_base.h
+++ include/time_test_base.h
@@ -4,6 +4,6 @@ namespace time_test {
     void time_test_base(const std::string &name, const std::string &fun1_name, const std::string &fun2_name,
                         const std::function<void(const interval::interval<int>&)> &fun1,
                         const std::function<void(const interval::interval<int>&)> &fun2);
-};
+}
 
 #endif //MATHINTERVAL_TIME_TEST_BASE_H
