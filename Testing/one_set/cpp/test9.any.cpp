c9ef4ddb5ca9efe50c0d9ed1e9c24861f517ec63
fedor <valyanskiy.f2009@yandex.ru>
2025-12-18 21:34:34 +0300

    update test9

--- Testing/one_set/cpp/test9.any.cpp
+++ Testing/one_set/cpp/test9.any.cpp
@@ -1,7 +1,9 @@
+#include <string>
 #include <gtest/gtest.h>
 #include <interval.h>
 #include <verifier.h>
-bool correct(const interval::interval<int> &a, const bool empty) {
+template <typename T>
+bool correct(const interval::interval<T> &a, const bool empty) {
     auto x = a.any();
     if (empty) {
         return !x.has_value();
@@ -12,6 +14,7 @@ bool correct(const interval::interval<int> &a, const bool empty) {
 
 TEST(ONE_SET, any) {
     interval::interval<int> a;
+    interval::interval<std::string> b;
     a.add_interval(1, 4);
     EXPECT_TRUE(correct(a, false)) << "error in step 1: " + a.to_string() +  "\n";
     a.clear();
@@ -44,4 +47,25 @@ TEST(ONE_SET, any) {
     a.clear();
     a.add_interval(interval::minimal<int>(), interval::maximal<int>());
     EXPECT_TRUE(correct(a, false)) << "error in step 11: " + a.to_string() +  "\n";
+
+    b.add_interval("a", "b");
+    EXPECT_TRUE(correct(b, false)) << "error in step 12: " + b.to_string() +  "\n";
+    b.clear();
+    b.add_interval("a", "aaa");
+    EXPECT_TRUE(correct(b, false)) << "error in step 13: " + b.to_string() +  "\n";
+    b.clear();
+    b.add_interval("a", "aa");
+    b.add_interval("aa", "aaa");
+    EXPECT_TRUE(correct(b, true )) << "error in step 14: " + b.to_string() +  "\n";
+    b.clear();
+    b.add_interval("a", "z");
+    EXPECT_TRUE(correct(b, false)) << "error in step 15: " + b.to_string() +  "\n";
+    b.clear();
+    EXPECT_TRUE(correct(b, true )) << "error in step 16: " + b.to_string() +  "\n";
+    b.add_interval("a", interval::maximal<std::string>());
+    EXPECT_TRUE(correct(b, false)) << "error in step 17: " + b.to_string() +  "\n";
+    b.clear();
+    b.add_interval(interval::minimal<std::string>(), interval::maximal<std::string>());
+    EXPECT_TRUE(correct(b, false)) << "error in step 18: " + b.to_string() +  "\n";
+
 }
