5666d091a72107b47a2fc10ba0f45bbe668e46a9
Fedoracora <valyanskiy.f2009@yandex.ru>
2025-12-25 20:46:20 +0300

    Update register format

--- src/register.cpp
+++ src/register.cpp
@@ -1,27 +1,41 @@
-#include <iostream>
-#include <verifier.h>
-#include <register.h>
 #include <additional_test_tools.h>
+#include <iostream>
+#include <register.h>
+#include <verifier.h>
 
 int main(const int argc, const char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
-        return 1;
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    return 1;
+  }
+  verify::line_checker line(verify::read_file(argv[1]));
+  auto a = line.get();
+  int now = -1;
+  bool changed = false;
+  int current_len = 0;
+  formatter::vec_reset();
+  while (a != std::nullopt) {
+    for (auto i = 0; i < formatter::tests.size(); i++) {
+      if (verify::same(a.value(),
+                       "===== " + formatter::tests[i].name + " =====", true,
+                       true, false))
+        now = i;
+      if (verify::same(a.value(),
+                       "===== end of " + formatter::tests[i].name + " =====",
+                       true, true, false))
+        changed = true, ++current_len;
     }
-    verify::line_checker line(verify::read_file(argv[1]));
-    auto a = line.get();
-    int now = -1;
-    bool changed = false;
-    while (a != std::nullopt) {
-        for (auto i = 0; i < formatter::tests.size(); i++) {
-            if (verify::same(a.value(), "===== " + formatter::tests[i].name + " =====", true, true, false)) now = i;
-            if (verify::same(a.value(), "===== end of " + formatter::tests[i].name + " =====", true, true, false)) changed = true;
-        }
-        if (changed) {
-        }
-        else if (now != -1) formatter::tests[now].func(a.value());
-        a = line.get();
-        if (changed) now = -1, changed = false;
+    if (current_len >= 1) {
+      current_len -= 1;
+      std::cout << verifier_tests::to_table(formatter::vec, formatter::HEADER)
+                << std::endl;
+      formatter::vec_reset();
     }
-    std::cout << verifier_tests::to_table(formatter::vec, formatter::HEADER);
+    if (changed) {
+    } else if (now != -1)
+      formatter::tests[now].func(a.value());
+    a = line.get();
+    if (changed)
+      now = -1, changed = false;
+  }
 }
