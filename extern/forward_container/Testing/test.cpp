ca355a3d98cf877da33c65d5d2d871727f0e05e7
fedoracora <valyanskiyfedor@gmail.com>
2026-02-17 09:02:02 +0300

    fully write forward_container.h

--- extern/forward_container/Testing/test.cpp
+++ extern/forward_container/Testing/test.cpp
@@ -1,5 +1,19 @@
 #include <gtest/gtest.h>
+#include <string>
 #include <forward_container/forward_container.h>
 TEST(FORWARD_CONTAINER, all_tests) {
+    const std::string using_s = "abc";
+    fbp::forward_container cont(using_s);
+    EXPECT_TRUE(cont.call() == using_s && cont < std::string("b"))
+            << "error in step 1: lvalue with call\n";
+    EXPECT_TRUE(std::move(cont).get() == using_s)
+            << "error in step 2: get from lvalue\n";
+    std::string s = using_s;
+    cont = fbp::forward_container(std::move(s));
+    s = "a";
+    EXPECT_TRUE(cont.call() == using_s && cont < std::string("b"))
+            << "error in step 3: rvalue with call\n";
+    EXPECT_TRUE(std::move(cont).get() == using_s && s == "a")
+            << "error in step 4: get from rvalue\n";
 
 }
--- extern/forward_container/include/forward_container/forward_container.h
+++ extern/forward_container/include/forward_container/forward_container.h
@@ -1,7 +1,5 @@
 #ifndef FORWARD_CONTAINER_H
 #define FORWARD_CONTAINER_H
-#include <algorithm>
-#include <optional>
 namespace fbp {
     template <typename T>
     struct forward_container {
@@ -23,13 +21,12 @@ namespace fbp {
             }
             return *this;
         }
-        [[nodiscard]] bool operator<(const forward_container &other) const {
-            return *link < *other.link;
+        [[nodiscard]] auto operator<=>(const forward_container &other) const {
+            return *link <=> *other.link;
         }
-        [[nodiscard]] bool operator==(const forward_container &other) const {
-            return *link == *other.link;
+        [[nodiscard]] auto operator<=>(const T &other) const {
+            return *link <=> other;
         }
-        [[nodiscard]] bool operator<=>(const forward_container &other) const = default;
         const T &call() const { return *link; }
         [[nodiscard]] T get() && {
             if (val) {
@@ -47,49 +44,5 @@ namespace fbp {
         T *val;
         const T *link;
     };
-    // template <typename U>
-    // class forward_container {
-    //     using T = std::remove_reference_t<U>;
-    // public:
-    //     explicit forward_container(U&& value) {
-    //         if constexpr (std::is_lvalue_reference_v<U>) {
-    //             ptr_ = &value;
-    //         } else {
-    //             obj_ = std::move(value);
-    //             ptr_ = &*obj_;
-    //         }
-    //     }
-    //
-    //     forward_container(const forward_container&) = delete;
-    //     forward_container& operator=(const forward_container&) = delete;
-    //
-    //     forward_container(forward_container&& other) noexcept
-    //         : obj_(std::move(other.obj_))
-    //     {
-    //         if (obj_) {
-    //             ptr_ = &*obj_;
-    //         } else {
-    //             ptr_ = other.ptr_;
-    //         }
-    //     }
-    //
-    //     forward_container& operator=(forward_container&&) noexcept = default;
-    //
-    //     decltype(auto) get() && {
-    //         if constexpr (std::is_lvalue_reference_v<U>) {
-    //             return *ptr_;            // T&
-    //         } else {
-    //             return std::move(*ptr_); // T&&
-    //         }
-    //     }
-    //
-    //     const T& call() const {
-    //         return *ptr_;
-    //     }
-    //
-    // private:
-    //     std::optional<T> obj_;
-    //     T* ptr_;
-    // };
 }
 #endif // FORWARD_CONTAINER_H
